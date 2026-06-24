177dd923ccd641489565501fc714ba230228348c
arnoldalias3-coder <arnoldalias3@gmail.com>
2026-06-24 10:47:19 +0530

    update cors for vercel frontend

--- server.js
+++ server.js
@@ -8,7 +8,28 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-app.use(cors());
+// Allowed frontend URLs
+const allowedOrigins = [
+  "http://localhost:5173",
+"https://task-manager-frontend-mu-lyart.vercel.app"];
+
+// CORS setup
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin, like Thunder Client/Postman
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
